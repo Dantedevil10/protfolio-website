608921c0d843d452a174e0ed16fd7ca5b4196c7b
Dantedevil10 <jonhjooj2907@gmail.com>
2024-07-11 19:17:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('pot').addEventListener('click', function() {
+    document.getElementById('s-s').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('wai').addEventListener('click', function() {
+    document.getElementById('f-s').scrollIntoView({ behavior: 'smooth' });
+});
